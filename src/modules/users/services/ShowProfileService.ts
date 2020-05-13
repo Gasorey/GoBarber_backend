e5f386aa1f7eae895116c7f05261dea213b02e4d
Gabriel Asorey <gasorey@gmail.com>
2020-05-13 12:30:03 -0300

    GoBarber beck-end

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,33 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+// import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { injectable, inject } from 'tsyringe';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  password?: string;
+  old_password?: string;
+}
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
