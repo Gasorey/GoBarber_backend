617735d3b4b4ffa5f84d74a516e55f644a05d533
Gabriel Asorey <gasorey@gmail.com>
2020-05-15 15:44:32 -0300

    GoBarber beck-end

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,13 +2,25 @@ import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 
 const providersRouter = Router();
 
 const providersController = new ProvidersController();
+const providersDayAvailability = new ProviderDayAvailabilityController();
+const providersMonthAvailability = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providersDayAvailability.index,
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providersMonthAvailability.index,
+);
 
 export default providersRouter;
