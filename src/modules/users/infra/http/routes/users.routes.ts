e537a09b9dfe6536567ac59185c894b80e57274d
Gabriel Asorey <gasorey@gmail.com>
2020-05-18 14:39:10 -0300

    GoBarber validation

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { Joi, celebrate, Segments } from 'celebrate';
 import uploadConfig from '@config/upload';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
@@ -11,7 +12,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      password: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
