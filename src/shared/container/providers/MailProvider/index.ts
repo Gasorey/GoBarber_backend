6185aff062fe6b92580e90c449a89be981b21d76
Gabriel Asorey <gasorey@gmail.com>
2020-05-20 19:58:06 -0300

    GoBarber Containers Refactored

---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,14 @@
+import { container } from 'tsyringe';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import mailConfig from '@config/mail';
+import EtherealMailProvider from './implementations/EtherealMailProvider';
+import SESMailProvider from './implementations/SESMailProvider';
+
+const providers = {
+  etherial: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  providers[mailConfig.driver],
+);
